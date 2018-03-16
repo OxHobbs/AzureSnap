d1ce22f90c3d410665f3241946b80f564d3cf327
Austin Hobbs <austin.hobbs@microsoft.com>
2018-03-15 22:56:19 -0500

    writevmbackup

--- helpers/Write-VMBackupConfig.ps1
+++ helpers/Write-VMBackupConfig.ps1
@@ -5,7 +5,7 @@
     param
     (
         [Parameter(Mandatory)]
-        [String]
+        [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]
         $VM,
 
         [Parameter()]
@@ -13,7 +13,17 @@
         $Clobber
     )
 
+    $backupPath = "$PSScriptRoot\..\backup\$($VM.Name).json"
+    $json = ConvertTo-Json -InputObject $VM
 
+    $backupFileExists = Test-Path $backupPath
 
-
+    if (($Clobber -and $backupFileExists) -or (-not $backupFileExists))
+    {
+        $json | Out-File -FilePath $backupPath -Force 
+    }
+    else
+    {
+        throw "A state file already exists for $($vm.Name).  Use the Clobber switch if you wish to override"
+    }
 }
